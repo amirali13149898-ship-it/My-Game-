9f0b714b98a2c31e5b9d3a484f1a47ae1b08e47c
amirali13149898-ship-it <amirali13149898@gmail.com>
2026-08-26 11:32:53 +0330

    Update sound.js

--- assets/js/utils/sound.js
+++ assets/js/utils/sound.js
@@ -1,7 +1,27 @@
-const clickSound = new Audio('assets/sounds/click.wav');
-clickSound.volume = 0.45;
+// assets/js/utils/sound.js
+
+let clickSound = null;
+
+function getClickSound() {
+  if (!clickSound) {
+    clickSound = new Audio('./assets/sounds/click.wav'); // مسیر نسبی مطمئن‌تر
+    clickSound.volume = 0.45;
+    clickSound.preload = 'auto';
+  }
+  return clickSound;
+}
 
 export function playClick() {
-  clickSound.currentTime = 0;
-  clickSound.play().catch(() => {});
+  try {
+    const sound = getClickSound();
+    sound.currentTime = 0;
+    const promise = sound.play();
+    if (promise !== undefined) {
+      promise.catch(err => {
+        console.warn('صدا پخش نشد:', err.message);
+      });
+    }
+  } catch (err) {
+    console.warn('خطا در پخش صدا:', err);
+  }
 }
